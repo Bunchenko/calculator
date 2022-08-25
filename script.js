866b4ee5f1bdd6192b2f2eed6bfe7ca8a66b708c
Bunchenko <andrew.by.one@gmail.com>
2022-08-25 21:02:53 +0300

    feat: add deleteLastChar function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input');
-const numberButtons = document.querySelectorAll('#number');
-const dotButton = document.querySelector('#button')
+const numberButtons = document.querySelectorAll('#digit');
+const dotButton = document.querySelector('#dot-button');
+const acButton = document.querySelector('#ac-button');
+const ceButton = document.querySelector('#ce-button');
 
 let dotWasPressed = false; //flag
 
@@ -8,7 +10,7 @@ function expandInput() {
     input.value += event.target.textContent
 }
 
-function appendNumber() {
+function appendDigit() {
 
     if (input.value === '0' && event.target.textContent === '0') {
         return
@@ -16,6 +18,11 @@ function appendNumber() {
 
     expandInput()
 }
+
+// function appendDigitKeyboard() {
+//     if (event.code.slice(0, 5) !== 'Digit') {return}
+//     console.log(event.code)
+// }
 
 function appendDot() {
 
@@ -31,5 +38,11 @@ function appendDot() {
     dotWasPressed = true // changing flag
 }
 
-numberButtons.forEach(button => button.addEventListener('click', appendNumber));
-dotButton.addEventListener('click', appendDot)
+function deleteLastChar() {
+    input.value = input.value.slice(0, -1)
+}
+
+input.addEventListener('keyup', appendDigitKeyboard)
+numberButtons.forEach(button => button.addEventListener('click', appendDigit));
+dotButton.addEventListener('click', appendDot);
+ceButton.addEventListener('click', deleteLastChar);
